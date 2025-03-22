6f26ca098dda452d21571b0fb8007f5327b4fff3
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-21 22:48:42 -0700

    Actualizar descargas-tiktok.mjs

--- plugins/descargas-tiktok.mjs
+++ plugins/descargas-tiktok.mjs
@@ -7,27 +7,27 @@ export default {
     flags: [],
     exec: async (wss, { m, args, usedPrefix }) => {
         if (!args[0]) {
-            return wss.sendMessage(m.chat, `✰ Por favor, ingresa un enlace de TikTok.`, { quoted: m });
+            return wss.sendMessage(m.chat, { text: "✰ Por favor, ingresa un enlace de TikTok." }, { quoted: m });
         }
 
         try {
-            await wss.sendMessage(m.chat, `⏳ Espere un momento, estoy descargando su video...`, { quoted: m });
+            await wss.sendMessage(m.chat, { text: `${emoji} Espere un momento, estoy descargando su video...` }, { quoted: m });
 
-            const tiktokData = await tiktokdl(args[0]); // ¿Dónde está definida esta función?
+            const tiktokData = await tiktokdl(args[0]);
 
             if (!tiktokData || !tiktokData.data || !tiktokData.data.play) {
-                return wss.sendMessage(m.chat, "❀ Error: No se pudo obtener el video.", { quoted: m });
+                return wss.sendMessage(m.chat, { text: "❀ Error: No se pudo obtener el video." }, { quoted: m });
             }
 
             const videoURL = tiktokData.data.play;
 
             if (videoURL) {
-                await wss.sendFile(m.chat, videoURL, "tiktok.mp4", `❀ Aquí tienes tu video.`, { quoted: m });
+                await wss.sendFile(m.chat, videoURL, "tiktok.mp4", { caption: "❀ Aquí tienes tu video." }, { quoted: m });
             } else {
-                return wss.sendMessage(m.chat, "No se pudo descargar.", { quoted: m });
+                return wss.sendMessage(m.chat, { text: "No se pudo descargar." }, { quoted: m });
             }
         } catch (error1) {
-            return wss.sendMessage(m.chat, `Error: ${error1.message}`, { quoted: m });
+            return wss.sendMessage(m.chat, { text: `Error: ${error1.message}` }, { quoted: m });
         }
     }
 };
